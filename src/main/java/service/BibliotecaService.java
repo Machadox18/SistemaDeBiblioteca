39d5b48d98573f2ce4626ac9acf97e028d84ec40
Gabriel <gmdf10@gmail.com>
2026-03-31 16:54:08 -0300

    feat: adiciona estruturas de dados para gerenciamento de livros e autores

--- src/main/java/service/BibliotecaService.java
+++ src/main/java/service/BibliotecaService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Livro;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BibliotecaService {
+
+    private List<Livro> livros = new ArrayList<Livro>();
+    private Map<String, List<Livro>> autores = new HashMap<>();
 }
