388e355098ae7e183eecbbbf9a11eae2270da94f
Gabriel <gmdf10@gmail.com>
2026-03-31 17:07:44 -0300

    feat: cria os livros e os adiciona no service

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import model.Livro;
+import service.BibliotecaService;
+
 public class Main {
     public static void main(String[] args) {
 
+        BibliotecaService service = new BibliotecaService();
+
+        Livro l1 = new Livro("Clean Code", "Robert Martin", 120);
+        Livro l2 = new Livro("Effective Java", "John Smith", 190);
+        Livro l3 = new Livro("Java Básico", "Robert Martin", 80);
+
+        service.adicionarLivro(l1);
+        service.adicionarLivro(l2);
+        service.adicionarLivro(l3);
     }
 }
